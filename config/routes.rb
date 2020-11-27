133f7115507c3ebb350abc429201b19656b18b52
miho.furukawa <homiwakarufu@gmail.com>
2020-11-27 07:49:45 +0000

    usersコントローラー編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   root to:'toppages#index'
-  
+  get 'signup', to: 'users#new'
+   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'users#new'
+   get 'signup', to: 'users#new'
   
   
   resources :users, only: [:new, :create] do
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
       get 'mypage'
     end
   end
-  resources :users
-  resources :messages, only: [:index, :creste,:new, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :new, :create]
+  resources :messages, only: [:index, :create,:new, :edit, :update, :destroy]
 end
 
